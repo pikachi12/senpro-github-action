09a2787f7bfc45197cef9f1b75eaa04a8ebebbac
navika <navikaberliandarihadatulaisya@mail.ugm.ac.id>
2025-03-13 09:48:15 +0700

    case 3

--- src/pages/index.js
+++ src/pages/index.js
@@ -30,7 +30,7 @@ export default function Home() {
 
         <div>
           <h5 className={inter.className}>
-            [Nama] | [NIU]
+            Navika Berlianda Rihadatul'aisya' | 505243
           </h5>
         </div>
       </main>
